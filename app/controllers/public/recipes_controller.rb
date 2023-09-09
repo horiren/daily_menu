248ada7e862f0836a8181bc23c3284b62854ea24
horiren <138359070+horiren@users.noreply.github.com>
2023-09-09 04:14:38 +0000

    [Add]controller-setting

--- app/controllers/public/recipes_controller.rb
+++ app/controllers/public/recipes_controller.rb
@@ -1,8 +1,26 @@
 class Public::RecipesController < ApplicationController
   
+  def index
+  end 
+  
+  def new
+  end 
+  
   def show
     @recipe = Recipe.find(params[:id])
     @post_comment = PostComment.new
   end
+  
+  def edit
+  end 
+  
+  def create
+  end 
+  
+  def destroy
+  end 
+  
+  def update
+  end 
   
 end
